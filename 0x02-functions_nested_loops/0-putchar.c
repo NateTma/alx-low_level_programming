98e9b48bca3ed0a71a733067e8340d099ae7ef84
Natenael <nattymultamirat@gmail.com>
2023-07-20 06:30:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,11 @@ int _putchar(char c);
 
 int main(void)
 {
-	char c[8]="_putchar";
-	int i=0;
+	char c[8] = "_putchar";
+	int i = 0;
 
-	while (i<8){
+	while (i < 8)
+	{
 		_putchar(c[i]);
 		i++;
 	}
