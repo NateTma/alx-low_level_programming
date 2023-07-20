163e21559fd43af64305a17e5e4178e4c2c49948
Natenael <nattymultamirat@gmail.com>
2023-07-20 06:20:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include "main.h";
 
 int _putchar(char c);
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
- */
-int main(char)
-{
-	char c[8] = "_putchar";
-	int i = 0;
+ * */
 
-	while (i < 8)
-	{
-	_putchar(c[i]);
-	i++;
+int main(void){
+	char c[8]="_putchar";
+	int i=0;
+
+	while (i<8){
+		_putchar(c[i]);
+		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
